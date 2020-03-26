78d311b898a76ecd2d7db53246cd9e838d3beadb
git_wzh <git_wzh@kedian.com>
2020-03-26 17:47:12 +0800

    链表算法

--- src/com/wuzh/algorithm/linkedlist/DeleteDuplicates_83.java
+++ src/com/wuzh/algorithm/linkedlist/DeleteDuplicates_83.java
@@ -16,8 +16,25 @@ package com.wuzh.algorithm.linkedlist;
  */
 public class DeleteDuplicates_83 {
 
-    public ListNode deleteDuplicates(ListNode head) {
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head==null){
+            return null;
+        }
+        //虚拟头节点
+        ListNode dummyNode=new ListNode(0);
+        dummyNode.next=head;
+        ListNode curNode=dummyNode;
+        while (curNode.next!=null){
+
+        }
 
         return null;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 2, 3, 3};
+        ListNode head = CreatePrintLinkedListUtil.createLinkedList(arr);
+        ListNode node = deleteDuplicates(head);
+        CreatePrintLinkedListUtil.printLinkedList(node);
     }
 }
