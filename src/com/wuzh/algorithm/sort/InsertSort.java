f263a5c320e0f65f471caa09706dcab5d76e333a
git_wzh <git_wzh@kedian.com>
2020-03-27 16:39:34 +0800

    十大排序

---
+++ src/com/wuzh/algorithm/sort/InsertSort.java
@@ -0,0 +1,12 @@
+package com.wuzh.algorithm.sort;
+
+/**
+ * @author wuzh
+ * @version V1.0
+ * @Package com.wuzh.algorithm.sort
+ * @Description: 插入排序
+ * @date 2020-03-27
+ */
+public class InsertSort {
+
+}
---
+++ src/com/wuzh/algorithm/sort/MergeSort.java
@@ -0,0 +1,12 @@
+package com.wuzh.algorithm.sort;
+
+/**
+ * @author wuzh
+ * @version V1.0
+ * @Package com.wuzh.algorithm.sort
+ * @Description: 归并排序
+ * @date 2020-03-27
+ */
+public class MergeSort {
+
+}
