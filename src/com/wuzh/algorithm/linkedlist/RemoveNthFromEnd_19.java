a8e7e30db81eb7e4a03fc42e20bf7227c2bbcdd0
lomo <lomo@qq.com>
2020-03-29 22:15:55 +0800

    查找表算法

---
+++ src/com/wuzh/algorithm/linkedlist/RemoveNthFromEnd_19.java
@@ -0,0 +1,28 @@
+package com.wuzh.algorithm.linkedlist;
+
+/**
+ * @author wzh
+ * @description 19. 删除链表的倒数第N个节点
+ * 给定一个链表，删除链表的倒数第 n 个节点，并且返回链表的头结点。
+ * <p>
+ * 示例：
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * <p>
+ * 说明：
+ * 给定的 n 保证是有效的。
+ * <p>
+ * 进阶：
+ * 你能尝试使用一趟扫描实现吗？
+ * @create 2020-03-29 11:17
+ */
+public class RemoveNthFromEnd_19 {
+
+    /**
+     * 利用双指针
+     */
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+
+        return null;
+    }
+}
