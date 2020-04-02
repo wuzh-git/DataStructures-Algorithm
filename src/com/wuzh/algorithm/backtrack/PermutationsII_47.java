2869c89469185b2612eaf7b3ac114a70b716e660
lomo <lomo@qq.com>
2020-04-02 22:10:19 +0800

    递归和回溯算法

---
+++ src/com/wuzh/algorithm/backtrack/PermutationsII_47.java
@@ -0,0 +1,27 @@
+package com.wuzh.algorithm.backtrack;
+
+import java.util.List;
+
+/**
+ * @author wzh
+ * @description 47. 全排列 II
+ * 给定一个可包含重复数字的序列，返回所有不重复的全排列。
+ *
+ * 示例:
+ *
+ * 输入: [1,1,2]
+ * 输出:
+ * [
+ *   [1,1,2],
+ *   [1,2,1],
+ *   [2,1,1]
+ * ]
+ * @create 2020-04-02 22:06
+ */
+public class PermutationsII_47 {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        return null;
+    }
+}
---
+++ src/com/wuzh/algorithm/backtrack/Permutations_46.java
@@ -0,0 +1,29 @@
+package com.wuzh.algorithm.backtrack;
+
+import java.util.List;
+
+/**
+ * @author wzh
+ * @description 46. 全排列
+ *  * 给定一个 没有重复 数字的序列，返回其所有可能的全排列。
+ *  *
+ *  * 示例:
+ *  * 输入: [1,2,3]
+ *  * 输出:
+ *  * [
+ *  *   [1,2,3],
+ *  *   [1,3,2],
+ *  *   [2,1,3],
+ *  *   [2,3,1],
+ *  *   [3,1,2],
+ *  *   [3,2,1]
+ *  * ]
+ * @create 2020-04-02 22:06
+ */
+public class Permutations_46 {
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+
+        return null;
+    }
+}
